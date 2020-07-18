ba4590e4d8014900b2876bd423abd101326d0013
Eliska Patockova <patockovaeliska@gmail.com>
2020-07-17 18:26:49 -0700

    set up server.js

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// User content from the public directory
+app.use(express.static("public"));
+
+//parse app 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
